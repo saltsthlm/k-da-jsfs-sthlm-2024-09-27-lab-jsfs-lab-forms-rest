eee8eb58ed5676a057d21d22885e441a8c08c89c
CarlUdo <carl@wbsweden.com>
2024-10-03 08:47:26 +0200

    Make
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ quo-gen-be/src/api/v1/users.ts
@@ -0,0 +1,14 @@
+export const db = [
+  {
+    username: 'leo@swederus.se',
+    password: '111'
+  },
+  {
+    username: 'antonberglund_92@hotmail.com',
+    password: '222'
+  },
+  {
+    username: 'carl@wbsweden.com',
+    password: '333'
+  }
+];
